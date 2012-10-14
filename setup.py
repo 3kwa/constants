3445ce33b19cb28cb232b56c6cb4daa876c0e8ea
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2012-10-14 15:33:38 +1100

    version bump (merge Python 3 branch) + BSD license

--- setup.py
+++ setup.py
@@ -3,11 +3,20 @@ from distutils.core import setup
 
 setup(
     name='constants',
-    version='0.3.0',
-    description='the simple way to deal with environment constants',
+    version='0.4.0',
+    description='The simple way to deal with environment constants.',
     long_description=open('README.rst').read(),
     author='Eugene Van den Bulke',
     author_email='eugene.vandenbulke@gmail.com',
     url='http://github.com/3kwa/constants',
-    py_modules=['constants']
+    py_modules=['constants'],
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries'
+        ],
 )
