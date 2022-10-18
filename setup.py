cee81c0f89ffbfc5f674a13cb26dde196c51b4bd
Andrew <paintstone@gmail.com>
2022-10-17 18:33:42 -0700

    updated setup.py, tox.ini for python 3.7

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ setup(
     version='0.6.0',
     description='The simple way to deal with environment constants.',
     long_description=open('README.rst').read(),
-    author='Eugene Van den Bulke',
-    author_email='eugene.vandenbulke@gmail.com',
+    author='Eugene Van den Bulke, Andrew Schell',
+    author_email='eugene.vandenbulke@gmail.com, andrewschell3@gmail.com',
     url='http://github.com/3kwa/constants',
     py_modules=['constants'],
     license='BSD',
+    install_requires=['tox', 'distutils', 'distutils.core', 'pytest', 'functools', 'unittest',
+                      'logging', 'warnings', 'configparser'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -21,6 +23,10 @@ setup(
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries'
         ],
 )
